e7d9470cfbe6fe43ad185ed1a44c44f98a09e883
unknown <nafis_fuad1234@yahoo.com>
2022-04-20 01:39:19 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_USER)
+.then(()=>console.log("DB Connection Succesfull"))
+.catch((err)=> {console.log(err);
+});
+
+app.use(express.json());
+app.use("/api/users", userRoute);
+
+
+
+app.listen(process.env.PORT || 5000, () => {
+    var port = process.env.PORT ;
+    console.log("backend server running on port" + port ); 
+});
